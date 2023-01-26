aea39cf0602b0ea8291676187a35955577778a17
Hidenari Nozaki <hidenari@gmail.com>
2023-01-26 20:23:27 +1300

    feat: add minimum styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 import WeeklyCalandar from './WeeklyCalandar';
 
+const guideId = process.env.REACT_APP_GUIDE_ID;
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Weekly Planner</h1>
-      <WeeklyCalandar />
-    </>
+    <div className="container mx-auto px-4 py-10">
+      <div className="md:grid md:grid-cols-4 md:gap-6">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:px-0">
+            <h1 className="text-3xl font-bold text-gray-900">Profile</h1>
+            <p className="mt-1 text-medium text-gray-600">User ID: {guideId}</p>
+          </div>
+        </div>
+        <div className="md:col-span-3 md:mt-0">
+          <h1 className="text-3xl font-bold text-gray-900">Weekly Planner</h1>
+          <WeeklyCalandar />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/WeeklyCalandar.tsx
+++ src/WeeklyCalandar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { addDays, getWeek, getYear, startOfWeek, format } from 'date-fns';
+import { addDays, startOfWeek, format } from 'date-fns';
 
 interface Availability {
   id: number;
@@ -11,23 +11,16 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 // TODO: Remove this later. This is only for development
 const guideId = process.env.REACT_APP_GUIDE_ID;
-console.log(`Initializing with Guide ID ${guideId}`);
 
-function calculateDays(today: Date) {
+function calculateDays(today: Date, selectedDate?: Date) {
   const start = startOfWeek(today);
-  const days = [start];
-  for (let i = 1; i <= 6; i++) {
-    days.push(addDays(start, i));
+  const days = [];
+  for (let i = 0; i < 7; i++) {
+    const day = addDays(start, i);
+    const selected = selectedDate?.getDate() === day.getDate();
+    days.push({ day, selected });
   }
   return days;
-}
-
-function calculateWeekIndex(today: Date) {
-  return getWeek(today);
-}
-
-function calculateYear(today: Date) {
-  return getYear(today);
 }
 
 function WeeklyCalandar() {
@@ -88,44 +81,73 @@ function WeeklyCalandar() {
   };
 
   return (
-    <div className="weekly-calendar mt-5">
-      <h1>
-        {calculateYear(today)} Week: {calculateWeekIndex(today)}
-      </h1>
+    <div className="weekly-calendar">
+      <p className="mt-2 text-medium text-gray-600">
+        Week {format(today, 'I')} in {today.getFullYear()}
+      </p>
 
-      <div className="mt-5 flex flex-row justify-between">
-        <button onClick={moveToPreviousWeek}>prev</button>
-        <button onClick={moveToNextWeek}>next</button>
+      <div className="mt-5 mb-5 flex flex-row justify-between">
+        <button
+          onClick={moveToPreviousWeek}
+          className="rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none"
+        >
+          PREV
+        </button>
+        <button
+          onClick={moveToNextWeek}
+          className="rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none"
+        >
+          NEXT
+        </button>
       </div>
 
       <ul className="flex flex-row justify-between">
-        {calculateDays(today).map((day) => (
-          <li className="day-element" key={+day} onClick={() => setSelectedDate(day)}>
-            {format(day, 'eee')} {day.getDate()}
+        {calculateDays(today, selectedDate).map(({ day, selected }) => (
+          <li
+            className={`day-element rounded-md border border-gray-300 py-4 px-5 text-medium font-light leading-4 text-gray-700 shadow-sm hover:bg-gray-50 ${
+              selected ? 'bg-slate-200' : ''
+            }`}
+            key={+day}
+            onClick={() => setSelectedDate(day)}
+          >
+            <span className="mb-3 text-sm font-bold">{format(day, 'MMM')}</span>
+            <br />
+            {format(day, 'eee')} {format(day, 'do')}
           </li>
         ))}
       </ul>
 
       {selectedDate ? (
-        <div className="mt-5">
+        <div className="mt-5 border p-3 bg-slate-200">
           <h3 className="font-bold">Add Availability</h3>
-          <p>{format(selectedDate, 'eeee d')}</p>
+          <p className="mt-1 mb-1 text-medium text-gray-600">{format(selectedDate, 'eeee MMM do')}</p>
           <form onSubmit={addAvailability} className="availability-form">
-            Start: <input type="time" ref={startAt} />
-            End: <input type="time" ref={endAt} />
-            <br />
-            <button type="submit">Add</button>
+            <span className="mr-5">
+              Start: <input type="time" ref={startAt} className="ml-2 rounded-md border border-gray-300" />
+            </span>
+            <span className="mr-5">
+              End: <input type="time" ref={endAt} className="ml-2 rounded-md border border-gray-300" />
+            </span>
+            <button
+              type="submit"
+              className="mt-2 rounded-md border border-gray-300 bg-white py-3 px-4 text-medium font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none"
+            >
+              ADD
+            </button>
           </form>
         </div>
       ) : null}
 
       <h3 className="mt-5 font-bold">Availabilities</h3>
-      <ul>
-        {availabilities.map((availability: Availability, i) => (
-          <li key={i}>
-            {availability.id}: {availability.startAt} - {availability.endAt}
-          </li>
-        ))}
+      <ul className="mt-2">
+        {availabilities
+          .sort((a, b) => +new Date(a.startAt) - +new Date(b.startAt))
+          .map((availability: Availability, i) => (
+            <li key={i} className="mb-1">
+              {format(new Date(availability.startAt), 'MMM do ccc a hh:mm')} -{' '}
+              {format(new Date(availability.endAt), 'a hh:mm')}
+            </li>
+          ))}
       </ul>
     </div>
   );
